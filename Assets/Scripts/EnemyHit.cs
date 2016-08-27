50c7661bae440f3535e022c00d4c0362f57c082d
Carlo "zED" Caputo <carlo.caputo@gmail.com>
2016-08-27 19:16:46 -0300

    breakable bricks can kill giants

---
+++ Assets/Scripts/EnemyHit.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using Gameplay.Unit;
+using Gameplay.Unit.Movement;
+
+public class EnemyHit : MonoBehaviour {
+
+  public double HitPoints = 100.0;
+
+	void Start () {
+  }
+	
+	void Update () {
+	
+	}
+
+  void OnCollisionEnter(Collision collision)
+  {
+    if (HitPoints <= 0) return;
+
+    foreach (ContactPoint contact in collision.contacts)
+    {
+      Rigidbody rb2 = contact.otherCollider.GetComponent<Rigidbody>();
+      if (!rb2) continue;
+      Debug.DrawRay(contact.point, -contact.normal * collision.relativeVelocity.magnitude, Color.white);
+      HitPoints -= collision.relativeVelocity.magnitude * rb2.mass;
+      Debug.Log(gameObject.name + " collided, hp " + HitPoints);
+      if (HitPoints <= 0 )
+      {
+        ParticleSystem ps = GetComponentInChildren<ParticleSystem>();
+        if(ps) ps.enableEmission = false;
+
+        GetComponent<NavMeshAgent>().enabled = false;
+        Destroy(GetComponent<BaseEnemy>());
+        Destroy(GetComponent<BaseMovement>());
+        Destroy(GetComponent<PathAgentController>());
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.mass = 2000;
+        rb.AddForce(collision.relativeVelocity * rb2.mass * 100, ForceMode.Impulse);
+        return;
+      }
+    }
+  }
+}
---
+++ Assets/Scripts/Sediment.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sediment : MonoBehaviour {
+  public enum State
+  {
+    PLACED,
+    DROP,
+    SEDIMENT
+  };
+  public double Delay = 1;
+  public State state = State.PLACED;
+  private double cooldown;
+
+  // Use this for initialization
+  void Start ()
+  {
+    cooldown = Time.time + Delay;
+    ChangeState(state, true);
+	}
+
+  void ChangeState(State s, bool startup = false)
+  {
+    if (!startup)
+      Debug.Log("target " + gameObject.name + " " + state + " => " + s);
+    switch ((state = s))
+    {
+      case State.SEDIMENT:
+        Destroy(GetComponent<Rigidbody>());
+        gameObject.AddComponent<NavMeshObstacle>().carving = true;
+        break;
+    }
+  }
+
+  // Update is called once per frame
+  void Update ()
+  {
+    if (state == State.DROP && GetComponent<Rigidbody>().IsSleeping())
+      ChangeState(State.SEDIMENT);
+  }
+
+  void OnCollisionEnter(Collision collision)
+  {
+    if (cooldown < Time.time && state == State.PLACED)
+    {
+      Debug.Log("collision " + gameObject.name + " with " + collision.other.name);
+      ChangeState(State.DROP);
+    }
+  }
+}
