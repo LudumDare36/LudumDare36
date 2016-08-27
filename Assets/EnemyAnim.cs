8a9df6cebd1521c80b2a4a737d1128d99111b477
Carlo "zED" Caputo <carlo.caputo@gmail.com>
2016-08-27 03:56:23 -0300

    robot warrior

---
+++ Assets/EnemyAnim.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAnim : MonoBehaviour {
+  private NavMeshAgent navmeshagent;
+  private Animator animator;
+
+  // Use this for initialization
+  void Start () {
+    navmeshagent = GetComponent<NavMeshAgent>();
+    animator = GetComponentInChildren<Animator>();
+  }
+
+  // Update is called once per frame
+  void Update () {
+    animator.SetFloat("vel", navmeshagent.velocity.magnitude);
+  }
+}
