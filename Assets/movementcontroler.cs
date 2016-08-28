af2810e3c6dcc0313ef1633f2ac509a0138f4f51
Ivan garcia filho <ivan.garcia.filho@hotmail.com>
2016-08-28 20:48:14 -0300

    Controles otimizados

--- Assets/movementcontroler.cs
+++ Assets/movementcontroler.cs
@@ -13,6 +13,16 @@ public class movementcontroler : MonoBehaviour {
 
 		transform.GetComponent<Rigidbody>().MovePosition(
 			(transform.position+(Camera.main.transform.forward
-                *(Time.deltaTime * Input.GetAxis("Vertical")))));
+                * (Time.deltaTime * (Input.GetAxis("Vertical") * 9f)))));
+
+        transform.forward
+            = Camera.main.transform.forward;
+
+        Camera.main.transform.Rotate(0f, (
+            Input.GetAxis("Horizontal")*90f) * Time.deltaTime, 0f);
+
+        Camera.main.transform.position
+            = Vector3.Lerp(Camera.main.transform.position, (transform.position
+                -(transform.forward*8f)+(Vector3.up*3f)), Time.deltaTime);
 	}
 }
