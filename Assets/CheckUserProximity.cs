7043544b95bec7274cb542bea19730ea375cb311
Ivan garcia filho <ivan.garcia.filho@hotmail.com>
2016-08-28 13:00:20 -0300

    novo personagem + animações

--- Assets/CheckUserProximity.cs
+++ Assets/CheckUserProximity.cs
@@ -2,31 +2,31 @@
 using System.Collections;
 
 public class CheckUserProximity : MonoBehaviour {
-    public Transform userAvatar;
+	public Transform userAvatar;
 
 	// Use this for initialization
 	void Start () {
-        userAvatar = Camera.main.transform;
-        StartCoroutine(CheckForAvatarProximity());
-    }
+		userAvatar = Camera.main.transform;
+		StartCoroutine(CheckForAvatarProximity());
+	}
 
-    IEnumerator CheckForAvatarProximity()
-    {
-        while (true)
-        {
-            yield return new WaitForFixedUpdate();
+	IEnumerator CheckForAvatarProximity()
+	{
+		while (true)
+		{
+			yield return new WaitForFixedUpdate();
 
-            if (Vector3.Distance(transform.position,userAvatar.position)>90f)
-            {
-                transform.GetComponent<ParticleSystem>().Stop();
+			if (Vector3.Distance(transform.position,userAvatar.position)>90f)
+			{
+				transform.GetComponent<ParticleSystem>().Stop();
 
-                yield return new WaitWhile(()
-                    => Vector3.Distance(transform.position, userAvatar.position) > 90f);
-            }
-            else
-            {
-                transform.GetComponent<ParticleSystem>().Play();
-            }
-        }
-    }
+				yield return new WaitWhile(()
+					=> Vector3.Distance(transform.position, userAvatar.position) > 90f);
+			}
+			else
+			{
+				transform.GetComponent<ParticleSystem>().Play();
+			}
+		}
+	}
 }
